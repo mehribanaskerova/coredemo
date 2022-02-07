c19bac3daa43ec4874d89471f6d44ef4554fec25
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2022-02-08 00:13:24 +0400

    first

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        EFCategoryRepository _eFCategoryRepository;
+        public CategoryManager(EFCategoryRepository eFCategoryRepository)
+        {
+            _eFCategoryRepository = eFCategoryRepository;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _eFCategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _eFCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _eFCategoryRepository.Update(category);
+        }
+
+        public Category GetbyID(int id)
+        {
+            return _eFCategoryRepository.GetbyID(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _eFCategoryRepository.GetListAll();
+        }
+    }
+}
---
+++ DataLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Insert(T t)
+        {
+            using var context = new ContextSQLServer();
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            using var context = new ContextSQLServer();
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetbyID(int id)
+        {
+            using var context = new ContextSQLServer();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            using var context = new ContextSQLServer();
+            return context.Set<T>().ToList();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new ContextSQLServer();
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
